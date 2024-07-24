ab44782f8148c9b5bcd839dc9b725046b7d69695
Matth-ewe-f <matthewtf73@gmail.com>
2024-07-24 17:25:41 -0400

    updated data for new ts types

--- server/scripts/updateData.js
+++ server/scripts/updateData.js
@@ -7,13 +7,13 @@ fs.readFile(filename, 'utf-8', (err, data) => {
     console.error(err);
   } else {
     let obj = JSON.parse(data);
-    obj.skills = [
-      { name: "Programming Languanges", items: ["C#", "C++", "C", "JS/TypeScript", "Java", "Python"] },
-      { name: "Game Audio Software", items: ["Wwise", "Unity Audio System"] },
-      { name: "Game Engines", items: ["Unity", "Godot", "RPG Maker VX Ace"] },
-      { name: "Languages", items: ["Conversational Japanese"] },
-      { name: "Relevant Coursework", items: ["Software System Design", "Object-Oriented Software Engineering", "Data Structures", "Operating Systems", "Sound Design for Games", "Linear Algebra", "Calculus"] }
-    ]
+    obj.skills.map(list => {
+      console.log(list.items);
+      list.items = list.items.map(item => {
+        return { text: item }
+      })
+      return list;
+    })
     const newData = JSON.stringify(obj);
     fs.writeFile(filename, newData, null, () => {
       console.log("updated data file");
